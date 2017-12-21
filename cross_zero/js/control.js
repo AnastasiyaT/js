1969ef0789b0445f1ccec0ce126d9d071edc3139
Имя <e-mail>
2017-12-21 17:19:53 +0300

    'id'

--- cross_zero/js/control.js
+++ cross_zero/js/control.js
@@ -7,7 +7,7 @@ button.addEventListener('click', GameTable);
 //создание div для игры
 var GameTable = function() {
 	for (var i = 0; i < 9; i++) {	
-	document.getElementByID('game-container').innerHTML+='<div class="cell"></div>';
+	document.getElementByID('field').innerHTML+='<div class="cell"></div>';
 	};
 	container.style.display = none;	
 }
